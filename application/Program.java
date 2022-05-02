bb9a91a1134131acb8af40dd784ce9702c62e21f
Matheus Medeiros <mathmed22@gmail.com>
2022-05-02 19:25:47 -0300

    Tratando a segunda exceção com if e else

--- application/Program.java
+++ application/Program.java
@@ -25,6 +25,25 @@ public class Program {
 		}else {
 			Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
 			System.out.println(reservation);
+			
+			System.out.println();
+			System.out.println("Enter the data to update: ");
+			System.out.println("Check-in date (dd/MM/yyyy):");
+			checkIn = sdf.parse(sc.next());
+			System.out.println("Check-out date (dd/MM/yyyy):");
+			checkOut = sdf.parse(sc.next());
+			
+			Date now = new Date();
+			
+			if(checkIn.before(now) || checkOut.before(now)) {
+				System.out.println("Error in reservation");
+			}
+			else if(!checkOut.after(checkIn)) {
+				System.out.println("Error in reservation: Check-out date must be after check-in date");
+			}else {
+				reservation.updateDates(checkIn, checkOut);
+				System.out.println(reservation);
+			}
 		}
 		
 		
