14b9a7bade0fae3246ea90a658fb7b1b7db3d364
Matheus Medeiros <mathmed22@gmail.com>
2022-05-22 17:59:35 -0300

    Tratando erros com exceções do java

--- entities/Reservation.java
+++ entities/Reservation.java
@@ -42,20 +42,20 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 	
-	public String updateDates(Date checkin, Date checkout) {
+	public void updateDates(Date checkin, Date checkout) {
 		
 		Date now = new Date();
 		
 		if(checkIn.before(now) || checkOut.before(now)) {
-			return "Reservation dates for update must be future dates";
+			throw new IllegalArgumentException("Reservation dates for update must be future dates");
 		}
 		 if(!checkOut.after(checkIn)) {
-			return "Error in reservation: Check-out date must be after check-in date";
+			throw new IllegalArgumentException("Check-out date must be after check-in date");
 		}
 		
 		this.checkIn = checkin;
 		this.checkOut = checkout;
-		return null;
+		
 	}
 	
 	@Override
